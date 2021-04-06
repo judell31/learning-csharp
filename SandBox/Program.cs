7e10567e3d038c3a4cb8baafb3a64a0add7a9fc1
jtdavis <justintdavis8@gmail.com>
2021-04-06 01:01:02 -0500

    Lesson 1 Learning C#

---
+++ SandBox/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace SandBox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isSunny = false;
+            String wear = "Wear sunglasses!";
+            String dontWear = "You don't need to wear sunglasses.";
+
+            bool atTheBeach = true;
+            String sunblock = "Sunblcok is a good idea.";
+            String noSunblock = "Sunblock isn't needed.";
+
+            bool goAnyway = true;
+            String going = "Awesome! Glad you don't mind clouds!";
+            String nextTime = "No worries! Hopefully next time we will have some sun!";
+
+            if (isSunny)
+            {
+                Console.WriteLine(wear);
+                if(atTheBeach)
+                {
+                    Console.WriteLine(sunblock);
+                }
+                else
+                {
+                    Console.WriteLine(noSunblock);
+                }
+            }
+            else
+            {
+                Console.WriteLine(dontWear);
+                if (goAnyway)
+                {
+                    Console.WriteLine(going);
+                }
+                else
+                {
+                    Console.WriteLine(nextTime);
+                }
+            }
+        }
+
+        /*
+         * If else statments
+         */
+        public static void conditionals()
+        {
+            int multi = 2 * 3;
+
+            if (multi == 56566)
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine("false");
+            }
+        }
+    }
+}
